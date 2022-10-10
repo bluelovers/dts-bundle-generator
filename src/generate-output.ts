7ddd9f1883f9eccddd6a5c697b364a3e6f6240e0
bluelovers <codelovers@users.sourceforge.net>
2022-10-11 07:14:42 +0800

    .

--- src/generate-output.ts
+++ src/generate-output.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import * as ts from 'typescript';
 
 import { hasNodeModifier } from './helpers/typescript';
--- src/helpers/typescript.ts
+++ src/helpers/typescript.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import * as ts from 'typescript';
 
 const namedDeclarationKinds = [
